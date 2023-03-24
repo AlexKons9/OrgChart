322afe086cf28d020eea55e36dd067a302c515c6
ioannis.mertzanakis <ioa.mertzanakis@gmail.com>
2023-03-24 12:12:10 +0200

    initialCommit

---
+++ ModelLibrary/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary.Models
+{
+    internal class Employee
+    {
+    }
+}
